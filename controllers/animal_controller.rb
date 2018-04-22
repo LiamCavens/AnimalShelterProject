27f70e19f425fd62b60044948d5f6d476cdd278f
Liam Cavens <liamcavens@googlemail.com>
2018-04-22 11:37:23 +0100

    working create animal page

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -7,15 +7,17 @@ get '/animals' do
     erb (:"animals/index")
 end
 
+get '/animals/new' do 
+    @animals = Animal.all
+    erb(:"animals/new")
+end
+
 get '/animals/:id' do
     @animal = Animal.find(params['id'].to_i)
     erb(:"animals/show")
 end
 
-get '/animals/new' do 
-    @animals = Animal.all
-    erb(:"animals/new")
-end
+
 
 post '/animals' do
     animal = Animal.new(params)
